57bc955414ecfe2abd7a164495e32f4b66cbccf8
Lugtu, Baron Olandria <baron.lugtu@sykes.com>
2022-04-17 02:44:19 +0800

    removed files

--- SimpleBankWeb.API/Program.cs
+++ SimpleBankWeb.API/Program.cs
@@ -43,13 +43,21 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("../swagger/v1/swagger.json", "SimpleBankingApi");
+});
 
 app.MapControllers();
 
