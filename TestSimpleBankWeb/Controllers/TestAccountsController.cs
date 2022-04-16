0701a09164d55ac817cdcf9cf7b98466d0475ecb
Lugtu, Baron Olandria <baron.lugtu@sykes.com>
2022-04-17 04:24:55 +0800

    added publish settings

---
+++ TestSimpleBankWeb/Controllers/TestAccountsController.cs
@@ -0,0 +1,86 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestSimpleBankWeb.Controllers
+{
+    public class TestAccountsController : Controller
+    {
+        // GET: TestAccountsController
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var list = new List<Account>();
+            return View(list);
+        }
+
+        // GET: TestAccountsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TestAccountsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TestAccountsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TestAccountsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TestAccountsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TestAccountsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TestAccountsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
