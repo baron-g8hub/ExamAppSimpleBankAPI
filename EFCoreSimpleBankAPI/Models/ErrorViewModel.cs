7ba7904a36a2967f09c5d897f384cf2dc5edfbfe
Lugtu, Baron Olandria <baron.lugtu@sykes.com>
2022-04-22 12:24:39 +0800

    removed d..s

--- EFCoreSimpleBankAPI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EFCoreSimpleBankAPI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
